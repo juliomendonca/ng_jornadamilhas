92e81ba5b290c306ff9bade3f7a92c6bdfb93b0c
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-23 17:28:53 -0300

    correcoes e melhorias

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { ModalComponent } from './shared/modal/modal.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
@@ -44,6 +45,11 @@ import { PerfilComponent } from './pages/perfil/perfil.component';
 import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interceptor';
 import { BuscaComponent } from './pages/busca/busca.component';
 import { PassagemComponent } from './shared/passagem/passagem.component';
+import { ParadasComponent } from './shared/form-busca/filtros-complementares/paradas/paradas.component';
+import { CompanhiasComponent } from './shared/form-busca/filtros-complementares/companhias/companhias.component';
+import { PrecosComponent } from './shared/form-busca/filtros-complementares/precos/precos.component';
+import { LabelComponent } from './shared/form-busca/filtros-complementares/label/label.component';
+import { FiltrosComplementaresComponent } from './shared/form-busca/filtros-complementares/filtros-complementares.component';
 
 @NgModule({
   declarations: [
@@ -68,7 +74,12 @@ import { PassagemComponent } from './shared/passagem/passagem.component';
     CadastroComponent,
     PerfilComponent,
     BuscaComponent,
-    PassagemComponent
+    PassagemComponent,
+    ParadasComponent,
+    CompanhiasComponent,
+    PrecosComponent,
+    LabelComponent,
+    FiltrosComplementaresComponent
   ],
   imports: [
     BrowserModule,
@@ -90,7 +101,8 @@ import { PassagemComponent } from './shared/passagem/passagem.component';
     MatAutocompleteModule,
     MatRadioModule,
     MatDividerModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSliderModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
--- frontend/src/app/core/services/form-busca.service.ts
+++ frontend/src/app/core/services/form-busca.service.ts
@@ -20,12 +20,16 @@ export class FormBuscaService {
       somenteIda,
       origem: new FormControl(null, [Validators.required]),
       destino: new FormControl(null, [Validators.required]),
-      tipo: new FormControl("Executiva"),
-      adultos: new FormControl(3),
+      tipo: new FormControl("Econômica"),
+      adultos: new FormControl(1),
       criancas: new FormControl(0),
-      bebes: new FormControl(1),
+      bebes: new FormControl(0),
       dataIda: new FormControl(null, [Validators.required]),
-      dataVolta
+      dataVolta,
+      conexoes: new FormControl(null),
+      companhias: new FormControl(null),
+      precoMin: new FormControl(null),
+      precoMax: new FormControl(null),
     })
     somenteIda.valueChanges.subscribe(somenteIda => {
       if(somenteIda){
@@ -95,6 +99,22 @@ export class FormBuscaService {
     const dataVoltaControl = this.obterControle<Date>('dataVolta');
     if (dataVoltaControl.value) {
       dadosBusca.dataVolta = dataVoltaControl.value.toISOString();
+    }
+    const conexoesControl = this.obterControle<number>('conexoes');
+    if(conexoesControl.value){
+      dadosBusca.conexoes = conexoesControl.value;
+    }
+    const companhiasControl = this.obterControle<number[]>('companhias');
+    if(companhiasControl.value){
+      dadosBusca.companhiasId = companhiasControl.value
+    }
+    const precoMinControl = this.obterControle<number>('precoMin')
+    if(precoMinControl.value){
+      dadosBusca.precoMin = precoMinControl.value
+    }
+    const precoMaxControl = this.obterControle<number>('precoMin')
+    if(precoMaxControl.value){
+      dadosBusca.precoMax = precoMaxControl.value
     }
     return dadosBusca
   }
