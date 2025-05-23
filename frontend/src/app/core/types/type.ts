4e5e3d3a143e88fd5ecffb873909b125a05d5140
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-23 17:09:26 -0300

    atualizacao front e inclusao do backend

---
+++ frontend/src/app/core/types/type.ts
@@ -0,0 +1,68 @@
+export interface Promocao {
+    id: number
+    destino: string
+    imagem: string
+    preco: number
+}
+
+export interface UnidadeFederativa {
+    id: number;
+    nome: string;
+    sigla: string;
+}
+
+export interface Depoimento {
+    id: number;
+    texto: string;
+    autor: string;
+    avatar: string;
+}
+
+export interface PessoaUsuaria {
+  nome: string;
+  nascimento: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+  senha: string;
+  cidade: string;
+  estado: UnidadeFederativa;
+  genero: string;
+}
+
+export interface Resultado {
+    paginaAtual: number;
+    ultimaPagina: number;
+    total: number;
+    precoMin: number;
+    precoMax: number;
+    resultado: Passagem[];
+}
+
+export interface Passagem{
+    tipo: string;
+    precoIda: number;
+    precoVolta: number;
+    taxaEmbarque: number;
+    conexoes: number;
+    tempoVoo: number;
+    origem: UnidadeFederativa;
+    destino: UnidadeFederativa;
+    companhia: Companhia;
+    dataIda: Date;
+    dataVolta: Date;
+    total: number;
+    orcamento: Orcamento[];
+}
+
+export interface Companhia{
+    id: string;
+    nome: string;
+}
+
+export interface Orcamento{
+    descricao: string;
+    preco: number;
+    taxaEmbarque: number;
+    total: number
+}
